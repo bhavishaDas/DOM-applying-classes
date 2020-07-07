3a5cbf5f65bf077ae0a8a4239a16aafd93fe706e
Bhavisha Das <bhivxx@gmail.com>
2020-07-07 13:43:49 +1200

    added functions as requested in sprint3

--- classes.js
+++ classes.js
@@ -1,25 +1,34 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
-function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+function start() {
+	// The first example is done for you. Uncomment the line below and reload the browser.
+	one();
+	two();
+	three();
+	makeVisible();
+	// Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
-  // First, we have to find the element:
-  var one = document.getElementById('one')
+function one() {
+	// First, we have to find the element:
+	const one = document.getElementById('one');
 
-  // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+	// Next, we apply a new CSS class to it:
+	one.classList.add('blue');
 }
 
-// CREATE FUNCTION two HERE
-
-// CREATE FUNCTION three HERE
-
-// CREATE FUNCTION makeVisible HERE
+function two() {
+	const two = document.getElementById('two');
+	two.classList.add('green');
+}
+function three() {
+	const three = document.getElementById('three');
+	three.classList.add('red');
+}
+function makeVisible() {
+	const invisible = document.getElementsByClassName('invisible')[0];
+	invisible.classList.add('visible');
+}
